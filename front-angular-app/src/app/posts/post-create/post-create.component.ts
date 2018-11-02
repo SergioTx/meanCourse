b1adc134e7ad4b84cf777b7282e48935fc90039f
SergioM <sergio.municio@geograma.com>
2018-11-02 17:18:41 +0100

    25

--- front-angular-app/src/app/posts/post-create/post-create.component.ts
+++ front-angular-app/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '../../../../node_modules/@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { Post } from '../post.model';
 
 @Component({
@@ -11,10 +13,13 @@ export class PostCreateComponent {
   enteredContent = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost(postInput: HTMLTextAreaElement) {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
