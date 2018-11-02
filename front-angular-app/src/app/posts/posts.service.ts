77c201264a26f78fe0ec5a2fb036ff3879bf28a2
SergioM <sergio.municio@geograma.com>
2018-11-02 17:43:11 +0100

    27

--- front-angular-app/src/app/posts/posts.service.ts
+++ front-angular-app/src/app/posts/posts.service.ts
@@ -1,18 +1,25 @@
-import { Post } from './post.model';
 import { Injectable } from '@angular/core';
+import { Post } from './post.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class PostsService {
     private posts: Post[] = [];
+    private postUpdated = new Subject<Post[]>();
 
     getPosts() {
         return [...this.posts];
     }
 
+    getPostUpdateListener() {
+        return this.postUpdated.asObservable(); // returns it as listener, but cannot emit
+    }
+
     addPost(title: string, content: string) {
         const post: Post = { title, content };
         this.posts.push(post);
+        this.postUpdated.next([...this.posts]);
     }
 }
