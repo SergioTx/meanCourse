748e8df70aca3d06f2572d9d1840282581320197
SergioTx <txitxo37@gmail.com>
2018-11-09 18:02:04 +0100

    39

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,20 @@ const express = require('express');
 
 const app = express();
 
+// no CORS protection
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requesteed-With, Content-Type, Accept'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
 app.use('/api/posts', (req, res, next) => {
     const posts = [{
         id: '1fghdfhdfghdfgh',
