5ae74aa753047b1cff7869de27a934763ded9e75
SergioTx <txitxo37@gmail.com>
2018-11-16 17:21:23 +0100

    55

--- front-angular-app/src/app/posts/posts.service.ts
+++ front-angular-app/src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -14,9 +15,19 @@ export class PostsService {
 
     getPosts() {
         // httpClient doesn't need to be unsubscribed
-        this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
-            .subscribe((postData) => {
-                this.posts = postData.posts;
+        this.http.get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
+            .pipe(map((postData) => {
+                return postData.posts
+                    .map(post => {
+                        return {
+                            title: post.title,
+                            content: post.content,
+                            id: post._id
+                        };
+                    });
+            }))
+            .subscribe(transformedPosts => {
+                this.posts = transformedPosts;
                 this.postsUpdated.next([...this.posts]);
             });
     }
